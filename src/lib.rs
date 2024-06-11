b8e7d4db947fff78c6de4ffd33d3d2a624e53e26
DavidZwitser <davidzwitser@gmail.com>
2024-06-11 17:01:30 +0200

    Layed down the groundwork

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use zed_extension_api::{self as zed, Result};
+
+struct BQNExtension;
+
+impl zed::Extension for BQNExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        _config: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let binary_name = "BQNLSP";
+        let path = worktree
+            .which(binary_name)
+            .ok_or_else(|| format!("{binary_name} executable not found in $PATH"))?;
+
+        Ok(zed::Command {
+            command: path,
+            args: vec![],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(BQNExtension);
